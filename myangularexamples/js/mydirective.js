a8885f43746e9041abedc9359b36f062b7739017
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2024-10-19 08:27:27 +0530

    Commit changes...

--- myangularexamples/js/mydirective.js
+++ myangularexamples/js/mydirective.js
@@ -1,6 +1,6 @@
 var module = angular.module('mymodule', [])
 
-module.directive('mydirective', function(){
+module.directive('mylanguage', function(){
 
 	var eng = "English";
 
@@ -11,9 +11,11 @@ module.directive('mydirective', function(){
 module.directive('mycitymenu', function(){
 
 	var mydropdown = "<select ng-model='city'>";
+
 	mydropdown += "<option>Patna</option>";	
 	mydropdown += "<option>Delhi</option>";	
 	mydropdown += "<option>Mumbai</option>";
+	mydropdown += "<option>Bangalore</option>";
 
 	return {template : mydropdown}	
 	
