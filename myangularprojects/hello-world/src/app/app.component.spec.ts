87cb80a48a1f8146b4e9bba2ad1c2b520d0707d5
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2026-02-12 18:10:44 +0530

    Commit changes..

--- myangularprojects/hello-world/src/app/app.component.spec.ts
+++ myangularprojects/hello-world/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'hello-world-app' title`, () => {
+  it(`should have the 'hello-world' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hello-world-app');
+    expect(app.title).toEqual('hello-world');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hello-world-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hello-world');
   });
 });
