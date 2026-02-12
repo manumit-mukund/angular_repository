e7c01343ccdbd4a2bf264462cb3913bcc79f4bfa
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2026-02-12 19:21:42 +0530

    Commit changes...

--- myangularprojects/hello-world/src/app/app.component.ts
+++ myangularprojects/hello-world/src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  /* . . . */
+  NgFor,
+  /* . . . */
+} from '@angular/common';
+
+export interface Item {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgFor, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
+
   title = 'hello-world';
+
+  items: Item[] = [
+    { id: 1, name: 'Item One' },
+    { id: 2, name: 'Item Two' },
+    { id: 3, name: 'Item Three' }
+  ];
+
 }
