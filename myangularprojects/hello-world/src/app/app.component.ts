d59389c53de1c798a1e0413a4b4e534426c84698
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2026-02-12 21:54:23 +0530

    Commit changes...

--- myangularprojects/hello-world/src/app/app.component.ts
+++ myangularprojects/hello-world/src/app/app.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {
-  /* . . . */
-  NgFor,
-  /* . . . */
-} from '@angular/common';
+import { NgFor} from '@angular/common';
 
 export interface Item {
 
