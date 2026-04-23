aca237ac309f41c2ae53b59cb61c7edf9b2d84f9
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2026-04-23 13:49:43 +0530

    Commit changes...

--- myangularprojects/angular_18/src/app/components/databinding/databinding.component.ts
+++ myangularprojects/angular_18/src/app/components/databinding/databinding.component.ts
@@ -1,25 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-databinding',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './databinding.component.html',
   styleUrl: './databinding.component.css'
 })
 export class DatabindingComponent {
 
   courseName: string = "Angular 18";
-
+  stateNane: string = "Maharashtra";
   inputType = "checkbox";
-
   myClassName: string = "bg-primary";
-
   rollNo: number = 101;
-
   isInidan: boolean = true;
-
   courseDate: Date = new Date();
+
+  firstName = signal("Manu Signal");
 
   constructor() {    
 
