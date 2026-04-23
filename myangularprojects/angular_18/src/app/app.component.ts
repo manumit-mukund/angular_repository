98354c68dd558c813600ae5c6b4f1e4ceb34a5de
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2026-04-23 09:55:45 +0530

    Commit changes...

--- myangularprojects/angular_18/src/app/app.component.ts
+++ myangularprojects/angular_18/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { DatabindingComponent } from './components/databinding/databinding.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AddEmployeeComponent, EmployeeListComponent],
+  imports: [RouterOutlet, AddEmployeeComponent, EmployeeListComponent, DatabindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
