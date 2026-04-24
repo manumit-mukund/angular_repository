e73e857b2beca22edb4cc894fed2a017415b9954
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2026-04-24 15:19:53 +0530

    Commit changes...

--- myangularprojects/angular_18/src/app/app.component.ts
+++ myangularprojects/angular_18/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
 import { DatabindingComponent } from './components/databinding/databinding.component';
+import { StructuralDirComponent } from './components/structural-dir/structural-dir.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AddEmployeeComponent, EmployeeListComponent, DatabindingComponent],
+  imports: [RouterOutlet, AddEmployeeComponent, EmployeeListComponent, DatabindingComponent, StructuralDirComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ myangularprojects/angular_18/src/app/components/structural-dir/structural-dir.component.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-structural-dir',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './structural-dir.component.html',
+  styleUrl: './structural-dir.component.css'
+})
+export class StructuralDirComponent {
+
+  isDiv1Visible: boolean = true;
+
+  showDiv1() {
+
+    this.isDiv1Visible = true;
+  }
+
+
+  hideDiv1() {
+
+    this.isDiv1Visible = false;
+
+  }
+
+
+}
